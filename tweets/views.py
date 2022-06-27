2cb0252cf706932ab89bf0253ee05ff1ee51d41c
= <=>
2022-06-27 15:17:35 +0200

    dynamic routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse, JsonResponse
 
-# Create your views here.
+from tweets.models import Tweet
+
+def home_view(request, *args, **kwargs):
+   return HttpResponse("<h1>Ololo</h1>")
+
+def tweet_detail_view(requets, tweet_id, *args, **kwargs):
+   data = {
+      "tweet_id": tweet_id,
+      # "image": obj.image.url,
+   }
+   status = 200
+   try:
+      obj = Tweet.objects.get(pk=tweet_id)      
+      data["content"] = obj.content
+   except:
+      data['message'] = 'Not found'
+      status = 404
+      raise Http404(f"No item in DB with {tweet_id}")
+
+   return JsonResponse(data, status=status)
