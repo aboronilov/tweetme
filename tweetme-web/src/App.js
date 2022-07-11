76c78502766a678d1dd92b48a76b32aba9c0ff3a
= <=>
2022-07-11 10:46:58 +0200

    fetch tweets from API

--- tweetme-web/src/App.js
+++ tweetme-web/src/App.js
@@ -1,37 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+
+function loadTweets(callback) {
+   const xhr = new XMLHttpRequest()
+   const method = 'GET' // "POST"
+   const url = "http://127.0.0.1:8000/api/tweets/"
+   const responseType = "json"
+   xhr.responseType = responseType
+   xhr.open(method, url)
+   xhr.onload = function () {
+      callback(xhr.response, xhr.status)
+   }
+   xhr.onerror = function (err) {
+      console.log(err)
+      callback({"message": "The request has an error"}, 400)
+   }
+   xhr.send()
+}
 
 
 function App() {
-  const [tweets, setTweets] = useState([{content: 123}])
-  useEffect(()=>{
-    const tweets = [{content: 123}, {content: "hello world"}]
-    setTweets(tweets)
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {tweets.map((tweet, index)=>{
-            return <li>{tweet.content}</li>
-          })}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [tweets, setTweets] = useState([])
+   useEffect(() => {
+      const myCallback = (response, status) => {
+         console.log(response, status)
+         if (status === 200) {
+            setTweets(response)
+         }
+      }
+      loadTweets(myCallback)
+   }, [])
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <p>
+               {tweets.map((tweet, index) => {
+                  return <li>{tweet.content}</li>
+               })}
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
